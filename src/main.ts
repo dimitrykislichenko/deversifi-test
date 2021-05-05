fb96449366775cb9d2be4e1203aa6de00cf65009
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2021-05-05 18:53:08 +0300

    Init bot and start trading

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { Bot } from "./bot";
+import { Wallet } from "./wallet";
+
+// Create a new wallet with initi value of ETH and USDT
+const wallet = new Wallet();
+wallet.inc("ETH", 10);
+wallet.inc("USD", 20000);
+
+// Create a new bot and start trading
+const bot = new Bot(wallet);
+bot.start();
